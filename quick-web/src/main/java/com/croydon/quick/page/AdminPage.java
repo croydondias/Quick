f445ee87d409bbb6356ae5dea4ed30b13f661809
Croydon Dias <croydondias@gmail.com>
2015-07-13 19:07:03 +0100

    Remove commented code

--- quick-web/src/main/java/com/croydon/quick/page/AdminPage.java
+++ quick-web/src/main/java/com/croydon/quick/page/AdminPage.java
@@ -2,25 +2,14 @@ package com.croydon.quick.page;
 
 import java.util.List;
 
-import org.apache.wicket.Application;
-import org.apache.wicket.authroles.authentication.AuthenticatedWebApplication;
-import org.apache.wicket.authroles.authentication.AuthenticatedWebSession;
-import org.apache.wicket.markup.head.CssHeaderItem;
-import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
-import org.apache.wicket.markup.repeater.RepeatingView;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.LoadableDetachableModel;
-import org.apache.wicket.model.PropertyModel;
-import org.apache.wicket.request.resource.CssResourceReference;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.wicketstuff.annotation.mount.MountPath;
 
-import com.croydon.quick.WicketWebApplication;
 import com.croydon.quick.domain.Employee;
 import com.croydon.quick.page.component.EmployeeForm;
 import com.croydon.quick.service.EmployeeService;
@@ -52,28 +41,5 @@ public class AdminPage extends WebPage implements AuthenticatedWebPage {
                 ));
         	}			
            });
-        
-//        RepeatingView listItems = new RepeatingView("listItems");
-//        add(listItems);
-//        for (Employee e : employees) {
-//        	listItems.add(new Label(listItems.newChildId(), e.getFirstName() + " " + e.getLastName()));
-//		}
-//        
-//        add(new ListView<Employee>("employees", employees) {
-//        	@Override
-//        	protected void populateItem(ListItem<Employee> item) {
-//        	   item.add(new Label("firstName", new PropertyModel(item.getModel(), "firstName")));
-//        	   item.add(new Label("lastName", new PropertyModel(item.getModel(), "lastName")));
-//        	}			
-//           });
     }
-    
-//    @Override
-//    public void renderHead(IHeaderResponse response) {
-//        super.renderHead(response);
-//        CssResourceReference cssResourceReference = new CssResourceReference(
-//                WicketWebApplication.class, "example.css");
-//        response.render(CssHeaderItem.forReference(cssResourceReference));
-//    }
-
 }
