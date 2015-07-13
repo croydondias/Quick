774093d6871d61d49d4c4310b9f43c452ee2b8b2
Croydon Dias <croydondias@gmail.com>
2015-07-14 00:41:42 +0100

    Remove unused code

--- quick-web/src/main/java/com/croydon/quick/WicketWebApplication.java
+++ quick-web/src/main/java/com/croydon/quick/WicketWebApplication.java
@@ -1,11 +1,15 @@
 package com.croydon.quick;
 
+import org.apache.wicket.Component;
 import org.apache.wicket.Page;
+import org.apache.wicket.RestartResponseAtInterceptPageException;
+import org.apache.wicket.Session;
+import org.apache.wicket.authorization.Action;
 import org.apache.wicket.authorization.IAuthorizationStrategy;
 import org.apache.wicket.authroles.authentication.AbstractAuthenticatedWebSession;
 import org.apache.wicket.authroles.authentication.AuthenticatedWebApplication;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.request.component.IRequestableComponent;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -17,22 +21,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.wicketstuff.annotation.scan.AnnotatedMountScanner;
-import org.wicketstuff.rest.utils.mounting.PackageScanner;
-import org.apache.wicket.Component;
-import org.apache.wicket.RestartResponseAtInterceptPageException;
-import org.apache.wicket.Session;
-import org.apache.wicket.authorization.Action;
-import org.apache.wicket.request.Request;
-import org.apache.wicket.request.Response;
-import org.apache.wicket.request.component.IRequestableComponent;
 
 import com.croydon.quick.config.BasicAuthenticationSession;
-import com.croydon.quick.page.AdminPage;
 import com.croydon.quick.page.AuthenticatedWebPage;
 import com.croydon.quick.page.HomePage;
-import com.croydon.quick.page.PlayPage;
 import com.croydon.quick.page.SignInPage;
 
 @EnableConfigurationProperties
@@ -43,11 +36,6 @@ public class WicketWebApplication extends AuthenticatedWebApplication {
 	
 	@Autowired
     private ApplicationContext applicationContext;
-	
-//	@RequestMapping("/")
-//    String home() {
-//        return "index.html";
-//    }
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -73,20 +61,9 @@ public class WicketWebApplication extends AuthenticatedWebApplication {
 		return SignInPage.class;
 	}
 	
-	/**
-     * <ul>
-     * <li>making the wicket components injectable by activating the
-     * SpringComponentInjector</li>
-     * <li>mounting the test page</li>
-     * <li>logging spring service method output to showcase working
-     * integration</li>
-     * </ul>
-     */
     @Override
     protected void init() {
         super.init();
-        //Bootstrap.install(this);
-        //PackageScanner.scanPackage("com.croydon.quick.api.wicket");
         
         // This is so that pages are automatically mounts based on their annotations
         new AnnotatedMountScanner().scanPackage("com.croydon.quick.page").mount(this);
@@ -127,10 +104,6 @@ public class WicketWebApplication extends AuthenticatedWebApplication {
         
         // This is so that spring beans can be injected in wicket
         getComponentInstantiationListeners().add(new SpringComponentInjector(this, applicationContext));
-        
-//        mountPage("/admin.html", AdminPage.class);
-//        mountPage("/signin.html", SignInPage.class);
-        //mountPage("/play.html", PlayPage.class);
     }
 
 }
