44ca845df4e528d8276016c03fa5983bdd26d5e8
Croydon Dias <croydondias@gmail.com>
2015-07-14 21:51:15 +0100

    Refactored

--- quick-web/src/main/java/com/croydon/quick/page/component/EmployeeForm.java
+++ quick-web/src/main/java/com/croydon/quick/page/component/EmployeeForm.java
@@ -1,7 +1,6 @@
 package com.croydon.quick.page.component;
 
 import org.apache.log4j.Logger;
-import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.PasswordTextField;
@@ -11,7 +10,6 @@ import org.apache.wicket.model.IModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.croydon.quick.config.AppProperties;
 import com.croydon.quick.domain.Employee;
 import com.croydon.quick.exception.EmployeeDoesntExistException;
 import com.croydon.quick.page.AdminPage;
@@ -36,14 +34,7 @@ public class EmployeeForm extends StatelessForm {
         Label email = new Label("email");
         add(email);
         PasswordTextField password = new PasswordTextField("password");
-        //TextField password = new TextField("password");
         add(password);
-        
-        Employee employee = (Employee)model.getObject();
-        
-        getSession().bind();
-    	final String currentUserEmail = (String) getSession().getAttribute(AppProperties.CURRENT_USER);
-    	boolean matchesCurrentUser = employee.getEmail().equals(currentUserEmail);
         
         add(new Button("save") { 
 	            @Override
@@ -53,22 +44,7 @@ public class EmployeeForm extends StatelessForm {
 	                updateEmployeeInDatabase(editedEmployee);
 	                setResponsePage(AdminPage.class);
 	            } 
-        });
-        
-//        Button removeButton = new Button("remove") { 
-//            @Override 
-//            public void onSubmit() { 
-//            	LOG.info("REMOVE clicked");
-//            	Employee selected = (Employee) getForm().getModelObject();
-//            	employeeService.delete(selected.getId());
-//                setResponsePage(AdminPage.class); 
-//            } 
-//        };
-//        if (matchesCurrentUser) {
-//        	// Disable the remove button when the current user matches the employee item
-//        	removeButton.add(AttributeModifier.replace("disabled", "disabled"));
-//        }
-//        add(removeButton); 
+        }); 
 	}
 	
 	private void updateEmployeeInDatabase(Employee editedEmployee) {
